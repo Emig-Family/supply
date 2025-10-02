74a5daa67e32ba1137693b124bdf5db3dcffe4f0
Addison Emig <addison@kwila.cloud>
2025-10-02 03:58:48 -0400

    fix: use ID

--- site/src/content/config.ts
+++ site/src/content/config.ts
@@ -1,8 +1,9 @@
+import { glob } from 'astro/loaders';
 import { defineCollection, z } from 'astro:content';
 
 export const collections = {
   inventory: defineCollection({
-    type: "data",
+    loader: glob({ pattern: "*.yaml", base: "src/content/inventory/" }),
     schema: z.object({
       title: z.string(),
       description: z.string(),
